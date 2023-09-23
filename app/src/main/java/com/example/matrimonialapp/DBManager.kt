ed90e7207241fe85ca7f0c4784c7fd539faa4316
Sahil Jain <sahil4.jain@paytm.com>
2023-09-23 21:44:59 +0530

    changed app logo

---
+++ app/src/main/java/com/example/matrimonialapp/DBManager.kt
@@ -0,0 +1,21 @@
+package com.example.matrimonialapp
+
+import com.example.matrimonialapp.activity.MainActivity
+import com.example.matrimonialapp.db.AppDatabase
+import com.example.matrimonialapp.db.entity.UserEntity
+
+class DBManager {
+    companion object{
+        private val database = AppDatabase.getDatabase(MainActivity.appContext)
+        private val dao = database?.userDao()
+
+        fun insertUser(user: UserEntity) = dao?.insert(user)
+
+        fun insertUserList(users: List<UserEntity>) = dao?.insertUsers(users)
+
+        fun updateUser(user: UserEntity) = dao?.update(user)
+
+        fun getUsersList() = dao?.getAllEntities()
+
+    }
+}
