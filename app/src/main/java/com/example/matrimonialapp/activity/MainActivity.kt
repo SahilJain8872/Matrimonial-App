9c118fe496e5d3a3758f2fb8e9d935a03a2704eb
Sahil Jain <sahil4.jain@paytm.com>
2023-09-23 07:49:59 +0530

    added required packages

--- app/src/main/java/com/example/matrimonialapp/activity/MainActivity.kt
+++ app/src/main/java/com/example/matrimonialapp/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.matrimonialapp
+package com.example.matrimonialapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.matrimonialapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/matrimonialapp/repository/MainRepository.kt
@@ -0,0 +1,4 @@
+package com.example.matrimonialapp.repository
+
+class MainRepository {
+}
